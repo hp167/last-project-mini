5d721c33d4385a4feccea0db8742f34d81dad53f
hp167 <opriyanka.s123@gmail.com>
2026-07-04 22:56:24 +0530

    Initialize Express server with MongoDB connection

---
+++ backend/backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bookstore")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/books", require("./routes/bookRoutes"));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
